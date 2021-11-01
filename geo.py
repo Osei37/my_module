cb9075ade7f0c2081e3e38abc9af17b1d32e72a4
Osei <yamashita.osei@gmail.com>
2021-11-02 00:49:21 +0900

    add new function

--- geo.py
+++ geo.py
@@ -1,5 +1,16 @@
 import re
 
+# [sw_lon, sw_lat, nw_lon, ne_lat] 
+# -> [[s_lat, w_lon], [s_lat, e_lon], [n_lat, e_lon], [n_lat, w_lon]]
+def change_degrees(list):
+    result = []
+    sw = [list[1], list[0]]
+    se = [list[1], list[2]]
+    ne = [list[3], list[2]]
+    nw = [list[3], list[0]]
+    result = [sw, se, ne, nw]
+    return result
+
 
 # DMS to DEG
 def dms2deg(list):
@@ -24,4 +35,9 @@ def dms2deg(list):
     else:
         print('error')
         return list
+    return result
+
+# DEG to DMS
+def deg2dms(list):
+    result = []
     return result
