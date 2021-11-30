b98238c2b8fd53961f26a8617fd31b80cfd31194
Osei <yamashita.osei@gmail.com>
2021-11-30 23:23:56 +0900

    add get_center method

--- bbox.py
+++ bbox.py
@@ -2,10 +2,10 @@ class BBoxCoordinate():
     
     def __init__(self):
         self.corner_list = [
-            [], # [bottom_left_x,   bottom_left_y   ]
-            [], # [top_left_x,      top_left_y      ]
-            [], # [top_right_x,     top_right_y     ]
-            []  # [bottom_right_x,  bottom_right_y  ]
+            [None, None], # [bottom_left_x,   bottom_left_y   ]
+            [None, None], # [top_left_x,      top_left_y      ]
+            [None, None], # [top_right_x,     top_right_y     ]
+            [None, None]  # [bottom_right_x,  bottom_right_y  ]
         ]
 
     def set_bottom_left(self, coord):
@@ -28,8 +28,15 @@ class BBoxCoordinate():
         self.corner_list[3][0] = coord[0]
         self.corner_list[1][1] = coord[1]
 
-    def _entered_check(_list):
-        if not _list:
+    def _calc_center(self):
+        center = []
+        center.append((self.corner_list[0][0] + self.corner_list[2][0]) / 2)
+        center.append((self.corner_list[0][1] + self.corner_list[2][1]) / 2)
+        return center
+
+    def _entered_check(self, _xy):
+        # for i in _xy:
+        if not _xy:
             raise Exception('Not entered index exist.')
 
     def get_corner_list(self):
@@ -52,3 +59,8 @@ class BBoxCoordinate():
     def get_bottom_left(self):
         self._entered_check(self.corner_list[3])
         return self.corner_list[3]
+
+    def get_center(self):
+        for _list in self.corner_list:
+            self._entered_check(_list)
+        return self._calc_center()
